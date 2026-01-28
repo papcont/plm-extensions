b0bd360d31880ece2da2506ca5792058ebd9ba2d
papcont <153505747+papcont@users.noreply.github.com>
2026-01-28 13:33:13 +0100

    Update asset management workspace IDs

--- settings.js
+++ settings.js
@@ -62,13 +62,14 @@ exports.common = {
         sparePartsRequests     : 241,
         supplierPackages       : 147,
 
-        // Asset Management Workspaces
-        orderProjects          : 283,
-        orderProjectDeliveries : 279,
-        assets                 : 280,
-        assetItems             : 282,
-        assetServices          : 284,
-        serialNumbers          : 277,
+        
+         // Asset Management Workspaces
+        orderProjects          : 280,
+        orderProjectDeliveries : 276,
+        assets                 : 277,
+        assetItems             : 278,
+        assetServices          : 281,
+        serialNumbers          : 274,
 
     },
 
